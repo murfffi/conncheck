6148d74ce09bdc067e78d7742c2e431ce98c0160
Marin Nozhchev <murfffi@gmail.com>
2026-05-17 10:27:43 +0300

    feat: initial support for windows

--- conncheck.go
+++ conncheck.go
@@ -2,7 +2,9 @@ package conncheck
 
 import (
 	"crypto/tls"
+	"fmt"
 	"net"
+	"strconv"
 	"syscall"
 	"time"
 )
@@ -14,6 +16,22 @@ const (
 	StatusOpen
 	StatusNotOpen
 )
+
+func (s Status) String() string {
+	res := strconv.Itoa(int(StatusUnknown))
+	switch s {
+	case StatusUnknown:
+		return res + ": StatusUnknown"
+	case StatusOpen:
+		return res + ": StatusOpen"
+	case StatusNotOpen:
+		return res + ": StatusNotOpen"
+	default:
+		return res + ": unknown"
+	}
+}
+
+var _ fmt.Stringer = StatusOpen
 
 func Do(conn net.Conn) Status {
 	if tlsConn, ok := conn.(*tls.Conn); ok {
@@ -29,7 +47,7 @@ func Do(conn net.Conn) Status {
 	rawConn, err := sc.SyscallConn()
 	if err == nil {
 		err = tryPeek(rawConn)
-	} // else eans that the system handle is not set
+	} // else means that the system handle is not set
 
 	if err != nil {
 		return StatusNotOpen
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/murfffi/conncheck
 
 go 1.26
 
-require github.com/stretchr/testify v1.11.1
+require (
+	github.com/stretchr/testify v1.11.1
+	golang.org/x/sys v0.44.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
